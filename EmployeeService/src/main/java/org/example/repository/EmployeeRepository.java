94d00fe9a7aaf9f81f37baef9537aca7d09f84fc
YaseminCaymaz <yaseminkanmaz15@gmail.com>
2024-04-30 09:06:02 +0300

    security-yasemin

--- EmployeeService/src/main/java/org/example/repository/EmployeeRepository.java
+++ EmployeeService/src/main/java/org/example/repository/EmployeeRepository.java
@@ -3,5 +3,8 @@ package org.example.repository;
 import org.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findOptionalByAuthId(Long authId);
 }

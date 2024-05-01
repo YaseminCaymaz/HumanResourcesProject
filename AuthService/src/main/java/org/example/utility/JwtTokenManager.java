b5b0308b464fa60890da07126ec7dd45d469a9f4
EsraKaranfil <esrakaranfileb@gmail.com>
2024-05-01 23:41:37 +0300

    employeeService hata düzeltme

--- AuthService/src/main/java/org/example/utility/JwtTokenManager.java
+++ AuthService/src/main/java/org/example/utility/JwtTokenManager.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 public class JwtTokenManager {
-    @Value("${jwt.SECRET_KEY}")
+    @Value("${jwt.secretKey}")
     private String secretKey;
     @Value("${jwt.issuer}")
     private String issuer;

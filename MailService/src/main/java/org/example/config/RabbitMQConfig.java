4c4b9d2714b979670e57dbe76ab706e8ecd256ef
EsraKaranfil <esrakaranfileb@gmail.com>
2024-05-09 17:17:41 +0300

    gün sonu esra

--- MailService/src/main/java/org/example/config/RabbitMQConfig.java
+++ MailService/src/main/java/org/example/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package org.example.config;
 
-import io.lettuce.core.dynamic.annotation.Value;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;

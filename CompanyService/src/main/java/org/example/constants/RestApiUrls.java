b31ae1a0347aa25f0e9d2077b817fc04c72e28f8
EsraKaranfil <esrakaranfileb@gmail.com>
2024-05-17 00:37:58 +0300

    manager

--- CompanyService/src/main/java/org/example/constants/RestApiUrls.java
+++ CompanyService/src/main/java/org/example/constants/RestApiUrls.java
@@ -6,6 +6,7 @@ public class RestApiUrls {
     private static final String ROOT = DEV + VERSION;
 
     public static final String COMPANY = ROOT + "/company";
+    public static final String MANAGER = ROOT + "/manager";
 
     public static final String REGISTER = "/register";
     public static final String LOGIN = "/login";
